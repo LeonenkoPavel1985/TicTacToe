e13ae24efd6b3e56e99fa6ce2ae7f9050d1d0cb4
LeonenkoPavel1985 <77398240+LeonenkoPavel1985@users.noreply.github.com>
2021-07-14 18:28:19 +0700

    Add files via upload

---
+++ Logic.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2_TicTacToe
+{
+    class Logic
+    {
+        static char [] array = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' }; // создаем массив из десяти символов. 0 не используется.
+        //array [0] = 'v'; победа (victory).
+        //array [0] = 'd';  ничья (drow).
+        //array [0] = 'c' продолжение игры (continuation game).
+
+        // Создаем поле для игры.
+        public void Board()
+        {
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", array[1], array[2], array[3]);
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", array[4], array[5], array[6]);
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", array[7], array[8], array[9]);
+            Console.WriteLine("     |     |      ");
+        }
+        //Проверка на выигрыш игрока.
+        static void CheckWin()
+        {
+            // совпадение в первой строке (победа).
+            if (array[1] == array[2] && array[2] == array[3])
+            {
+                array[0] = 'v';
+                return;
+            }
+            // совпадение во второй строке (победа).
+            else if (array[4] == array[5] && array[5] == array[6])
+            {
+                array[0] = 'v';
+                return;
+            }
+            // совпадение во второй строке (победа).
+            else if (array[6] == array[7] && array[7] == array[8])
+            {
+                array[0] = 'v';
+                return;
+            }
+            // совпадение в первой колонке (победа).
+            else if (array[1] == array[4] && array[4] == array[7])
+
+            {
+                array[0] = 'v';
+                return;
+            }
+            // совпадение в второй колонке (победа).
+            else if (array[2] == array[5] && array[5] == array[8])
+            {
+                array[0] = 'v';
+                return;
+            }
+            // совпадение в третей колонке (победа).
+            else if (array[3] == array[6] && array[6] == array[9])
+            {
+                array[0] = 'v';
+                return;
+            }
+            // совпадение по первой диагонали (победа).
+            else if (array[1] == array[5] && array[5] == array[9])
+            {
+                array[0] = 'v';
+                return;
+            }
+            // совпадение по второй диагонали (победа).
+            else if (array[3] == array[5] && array[5] == array[7])
+            {
+                array[0] = 'v';
+                return;
+            }
+            // проверяем на ничью (не возможно определить победителя).
+            if (array[1] != '1' && array[2] != '2' && array[3] != '3' && array[4] != '4' && array[5] != '5' && array[6] != '6' && array[7] != '7' && array[8] != '8' && array[9] != '9')
+            {
+                array[0] = 'd';
+                return;
+            }
+            // продолжение игры.
+            else
+            {
+                array[0] = 'c';
+            }
+        }
+        public char StatusGame ()// статус игры.
+        {
+            return array[0];
+        }
+        // Проверка пустой позиции.
+        public bool EmptyPosition (int position)
+        {
+            if (array[position] != 'O' && array[position] != 'X')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void Moving (int position, char player)
+        {
+            array[position] = player;
+            CheckWin();
+        }
+    }  
+}
---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿/*Задание. Игра "Крестики-Нолики".
+Реализовать игру на C#. Отделить класс, отвечающий за логику игры от класса для отображения хода игры в консоли. 
+Сделать, постараться сделать, красивый консольный интерфейс. Игровое поле 3х3. Результат - репозиторий в своём аккаунте 
+на GitHub'е.*/
+
+using System;
+using System.Threading;
+
+namespace Task_2_TicTacToe
+{
+    class Program
+    {
+        static int player = 1; // по умолчанию устанавливаем первого игрока (крестики).
+        static int choice; // выбор хода.
+        static Logic game = new Logic(); // класс Logic.
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo key;
+            do
+            {
+                ScreenReload();
+                do
+                {
+                    key = Console.ReadKey(true);
+                    choice = key.KeyChar - 48;
+                } 
+                while (choice < 1 || choice > 9);
+                //  проверяем занята ли позиция.
+                if (game.EmptyPosition(choice))
+                {
+                    //  позиция свободна.
+                    if (player % 2 == 0)
+                    {
+                        game.Moving(choice, 'O');
+                    }
+                    else
+                    {
+                        game.Moving(choice, 'X');
+                    }
+                    player++;
+                }
+                else
+                {
+                    //  позиция занята.
+                    Console.SetCursorPosition(18, 27);
+                    Console.WriteLine("\n\tЗанятая клетка.");
+                    Thread.Sleep(2000);
+                }
+            } 
+            while (game.StatusGame() == 'g');
+            ScreenReload();
+            if (game.StatusGame() == 'v')
+            {
+                Console.SetCursorPosition(18, 27);
+                var winer = (player % 2) + 1;
+                var symbol = (winer % 2) == 0 ? 'O' : 'X';
+                Console.WriteLine($"Игрок {winer}: \"{symbol}\" выиграл.");
+            }
+            else
+            {
+                Console.SetCursorPosition(23, 27);
+                Console.WriteLine("Победила дружба.");
+            }
+            Console.ReadKey();
+        }
+        static void ScreenReload ()
+        {
+            Console.Clear(); //очистка консоли.
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("----------------------------------------------------------------");
+            Console.WriteLine("|--------------------Игра Крестики-Нолики.---------------------|");
+            Console.WriteLine("----------------------------------------------------------------");
+            Console.WriteLine("--Игрок 1 играет крестиками против Игрока 2 играющего ноликами--");
+            Console.WriteLine("----------------------------------------------------------------");
+            Console.ResetColor();
+            game.Board();
+            if (game.StatusGame() == 'g')
+            {
+                Console.SetCursorPosition(18, 27);
+                Console.Write("Ходит: ");
+                if (player % 2 == 0)
+                {
+                    Console.Write("Второй игрок: \"O\"");
+                }
+                else 
+                {
+                    Console.Write("Первый игрок: \"X\"");
+                }
+            }
+        }
+    }
+}
